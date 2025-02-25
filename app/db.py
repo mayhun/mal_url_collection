a26fb562acd0091974ee4ff57d96060076d938cb
may <mayhun28@gmail.com>
2025-02-25 10:11:40 +0900

    crc 값 추가 삽입

--- app/db.py
+++ app/db.py
@@ -1,6 +1,7 @@
 import pymysql
 import os
 from dotenv import load_dotenv
+from zlib import crc32
 
 load_dotenv(dotenv_path='../config/.env')
 
@@ -21,27 +22,29 @@ connection = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor
 )
 
+def calculate_crc32(url):
+    return crc32(url.encode()) & 0xffffffff
 
 def insert_malicious_url(url, source):
     try:
         with connection.cursor() as cursor:
+            url_crc = calculate_crc32(url)
+
             # 중복 확인
-            check_sql = "SELECT mal_id FROM mal_urls WHERE url = %s"
-            cursor.execute(check_sql, (url,))
+            check_sql = "SELECT mal_id FROM mal_urls WHERE url_crc = %s AND url = %s"
+            cursor.execute(check_sql, (url_crc, url))
             result = cursor.fetchone()
 
             # 없을 때만 삽입
             if result is None:
-                insert_sql = "INSERT INTO mal_urls (url, source) VALUES (%s, %s)"
-                cursor.execute(insert_sql, (url, source))
+                insert_sql = "INSERT INTO mal_urls (url, url_crc, source) VALUES (%s, %s, %s)"
+                cursor.execute(insert_sql, (url, url_crc, source))
                 connection.commit()
-                
                 return True
-
             else:
                 return False
-        
     except Exception as e:
         print(f"Error: {e}")
         return False
-    
+    finally:
+        connection.close()
